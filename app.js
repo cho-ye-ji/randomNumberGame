41b962318c28f7566f92b710031c1c4a3ea8c8e4
Cho ye ji <100740000+cho-ye-ji@users.noreply.github.com>
2023-10-10 15:38:29 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+const yourNumberInput = document.querySelector(".numberInputBox p input.yourNumber");
+const maxNumInput = document.querySelector(".numberInputBox .maxNumLine"); 
+const maxNumLine = maxNumInput.value; 
+const numberInputSubmit = document.querySelector(".numberInputSubmit");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNSME = "hidden";
+
+function HandleInputSubmit() {
+    const yourNumber = yourNumberInput.value;
+    const maxNumLine = maxNumInput.value; 
+    const randomNumber = Math.floor(Math.random() * maxNumLine);
+
+  if (yourNumber > randomNumber) {
+    greeting.innerText = `You chose: ${yourNumber}, the machine chose: ${randomNumber}.
+        You won!`;
+    greeting.classList.remove(HIDDEN_CLASSNSME);
+  } else if (yourNumber === randomNumber) {
+    greeting.innerText = `You chose: ${yourNumber}, the machine chose: ${randomNumber}.
+        You are same!`;
+    greeting.classList.remove(HIDDEN_CLASSNSME);
+  } else {
+    greeting.innerText = `You chose: ${yourNumber}, the machine chose: ${randomNumber}.
+        You lost!`;
+    greeting.classList.remove(HIDDEN_CLASSNSME);
+  }
+}
+
+
+numberInputSubmit.addEventListener("click", HandleInputSubmit);
+
+
+
